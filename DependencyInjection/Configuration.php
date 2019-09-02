fb5a429c430870d217de2263109c7a29a321c318
Aleksandr Klimenkov <alx.devel@gmail.com>
2019-09-02 15:07:49 +0300

    support symfony 4.3

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -14,8 +14,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('staffim_dto');
+        $treeBuilder = new TreeBuilder('staffim_dto');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
--- Event/PostMapEvent.php
+++ Event/PostMapEvent.php
@@ -2,12 +2,17 @@
 
 namespace Staffim\DTOBundle\Event;
 
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 use Staffim\DTOBundle\Model\ModelInterface;
 use Staffim\DTOBundle\DTO\Model\DTOInterface;
 
 class PostMapEvent extends Event
 {
+    /**
+     * @var string
+     */
+    private $name;
+
     /**
      * @var \Staffim\DTOBundle\Model\ModelInterface
      */
@@ -19,20 +24,36 @@ class PostMapEvent extends Event
     private $dto;
 
     /**
+     * @param string $name
      * @param \Staffim\DTOBundle\Model\ModelInterface $model
      * @param \Staffim\DTOBundle\DTO\Model\DTOInterface $dto
      */
-    public function __construct(ModelInterface $model, DTOInterface $dto)
+    public function __construct($name, ModelInterface $model, DTOInterface $dto)
     {
+        $this->name = $name;
         $this->model = $model;
         $this->dto = $dto;
     }
 
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return \Staffim\DTOBundle\Model\ModelInterface
+     */
     public function getModel()
     {
         return $this->model;
     }
 
+    /**
+     * @return \Staffim\DTOBundle\DTO\Model\DTOInterface
+     */
     public function getDTO()
     {
         return $this->dto;
